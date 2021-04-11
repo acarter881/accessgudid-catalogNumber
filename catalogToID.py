fd5cc79d2828eaa3405c333887549553c05253af
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-04-10 22:50:41 -0500

    Update catalogToID.py

--- catalogToID.py
+++ catalogToID.py
@@ -67,7 +67,7 @@ for i in range(len(lines)):
     # Create a variable for the main section of the webpage
     yo = searchResults.find_all('div', class_='xsmall-12 medium-6 columns')
     
-    # If the find_all method returns an empty list, append to dev_IDs. Otherwise, call the companyNames function
+    # If the find_all method returns an empty list or if the site suggests that you misspelled your query, append to dev_IDs. Otherwise, call the companyNames function.
     if yo == [] or soup.find_all('span', class_='spelling-correction'):
         dev_IDs.append((CAT_NUMBER, 'Not Found', 'Not Found'))
     else:
